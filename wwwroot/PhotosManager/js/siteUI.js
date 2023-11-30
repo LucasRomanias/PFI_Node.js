26f430e12387db93d595e666afc0dc792e29f88f
LucasRomanias <lucasromanias@gmail.com>
2023-11-30 16:28:52 -0500

    petit debut

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -4,12 +4,14 @@ Init_UI();
 
 function Init_UI() {
     renderLogin();
+    renderHeader();
     $('#aboutCmd').on("click", function () {
         renderAbout();
     });
+    $('#loginCmd').on("click", function () {
+        renderLogin();
+    });
 }
-
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Views rendering
@@ -26,9 +28,57 @@ function saveContentScrollPosition() {
 function restoreContentScrollPosition() {
     $("#content")[0].scrollTop = contentScrollPosition;
 }
-function updateHeader() {
-    //todo
+function updateHeader(string, name) {
+    $("#header") = empty();
+
+    $(".viewTitle") = (string)
+
+    
 }
+
+function renderHeader(){    
+    timeout();
+    saveContentScrollPosition();
+    eraseContent();
+
+    $("#header").append(
+        $(`
+        <span title="Liste des photos" id="listPhotosCmd">
+        <img src="images/PhotoCloudLogo.png" class="appLogo">
+         </span>
+        <span class="viewTitle">Liste des photos
+        <div class="cmdIcon fa fa-plus" id="newPhotoCmd" title="Ajouter une photo"></div>
+        </span>
+        <div class="headerMenusContainer">
+        <span>&nbsp;</span> <!--filler-->
+        <i title="Modifier votre profil">
+        <div class="UserAvatarSmall" userid="" id="editProfilCmd"
+        style="background-image:url('')"
+        title="Nicolas Chourot"></div>
+        </i>
+        <div class="dropdown ms-auto dropdownLayout">
+        <div data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="cmdIcon fa fa-ellipsis-vertical"></i>
+    </div>
+    <div class="dropdown-menu noselect">
+        <div class="dropdown-item" id="loginCmd">
+            <i class="menuIcon fa fa-sign-in mx-2"></i> Connexion
+        </div>
+        <div class="dropdown-divider"></div>
+
+        <div class="dropdown-item" id="aboutCmd">
+            <i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
+        </div>
+    </div>
+        </div>
+        </div>
+        
+        
+  
+        `))
+}
+
+
 function renderAbout() {
     timeout();
     saveContentScrollPosition();
@@ -55,10 +105,16 @@ function renderAbout() {
 }
 
 function renderLogin() {
+
+    let loginMessage = "";
+    let Email = "";
+    let EmailError = "";
+    let passwordError = "";
+
     timeout();
     saveContentScrollPosition();
     eraseContent();
-    UpdateHeader("À propos...", "about");
+    //UpdateHeader("À propos...", "about");
 
     $("#content").append(
         $(`
