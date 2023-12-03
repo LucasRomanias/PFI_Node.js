99624e2a6acc421f0ae994736d5255128f97e9dd
LucasRomanias <lucasromanias@gmail.com>
2023-12-03 12:29:02 -0500

    .

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -4,12 +4,19 @@ Init_UI();
 
 function Init_UI() {
     renderLogin();
+    
     renderHeader();
+
     $('#aboutCmd').on("click", function () {
         renderAbout();
     });
+
     $('#loginCmd').on("click", function () {
         renderLogin();
+    });
+
+    $('#createProfilCmd').on("click", function () {
+        renderRegister();
     });
 }
 
@@ -83,7 +90,7 @@ function renderAbout() {
     timeout();
     saveContentScrollPosition();
     eraseContent();
-    UpdateHeader("À propos...", "about");
+    //UpdateHeader("À propos...", "about");
 
     $("#content").append(
         $(`
@@ -147,3 +154,81 @@ function renderLogin() {
         
         `))
 }
+
+
+function renderRegister() {
+    timeout();
+    saveContentScrollPosition();
+    eraseContent();
+    //UpdateHeader("À propos...", "about");
+
+    $("#content").append(
+        $(`
+        <form class="form" id="createProfilForm"'>
+        <fieldset>
+        <legend>Adresse ce courriel</legend>
+        <input type="email"
+        class="form-control Email"
+        name="Email"
+        id="Email"
+        placeholder="Courriel"
+        required
+        RequireMessage = 'Veuillez entrer votre courriel'
+        InvalidMessage = 'Courriel invalide'
+        CustomErrorMessage ="Ce courriel est déjà utilisé"/>
+        <input class="form-control MatchedInput"
+        type="text"
+        matchedInputId="Email"
+        name="matchedEmail"
+        id="matchedEmail"
+        placeholder="Vérification"
+        required
+        RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+        InvalidMessage="Les courriels ne correspondent pas" />
+        </fieldset>
+        <fieldset>
+        <legend>Mot de passe</legend>
+        <input type="password"
+        class="form-control"
+        name="Password"
+        id="Password"
+        placeholder="Mot de passe"
+        required
+        RequireMessage = 'Veuillez entrer un mot de passe'
+        InvalidMessage = 'Mot de passe trop court'/>
+        <input class="form-control MatchedInput"
+        type="password"
+        matchedInputId="Password"
+        name="matchedPassword"
+        id="matchedPassword"
+        placeholder="Vérification" required
+        InvalidMessage="Ne correspond pas au mot de passe" />
+        </fieldset>
+        <fieldset>
+        <legend>Nom</legend>
+        <input type="text"
+        class="form-control Alpha"
+        name="Name"
+        id="Name"
+        placeholder="Nom"
+        required
+        RequireMessage = 'Veuillez entrer votre nom'
+        InvalidMessage = 'Nom invalide'/>
+        </fieldset>
+        <fieldset>
+        <legend>Avatar</legend>
+        <div class='imageUploader'
+        newImage='true'
+        controlId='Avatar'
+        imageSrc='images/no-avatar.png'
+        waitingImage="images/Loading_icon.gif">
+        </div>
+        </fieldset>
+        <input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
+        </form>
+        <div class="cancel">
+        <button class="form-control btn-secondary" id="abortCmd">Annuler</button>
+        </div>
+        `))
+}
+
