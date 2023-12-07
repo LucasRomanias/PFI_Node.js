65c259f240e87d7db88631b0c9aaa4d70d216793
LucasRomanias <lucasromanias@gmail.com>
2023-12-06 22:21:30 -0500

    .

--- wwwroot/PhotosManager/js/api.js
+++ wwwroot/PhotosManager/js/api.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////// API photos_APIs call ///////////////////////////////////////////////////////
 
-const serverHost = "http://localhost:5000";
+const serverHost = "https://alike-wooded-sphere.glitch.me";
 const photos_API = "/api/photos";
 
 class API {
